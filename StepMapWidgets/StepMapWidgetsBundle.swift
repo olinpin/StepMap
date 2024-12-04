e0d7c72117ea115760e06766316ccf3d4c16a869
oliverhnat <oliver.hnat@gmail.com>
2024-12-04 15:08:52 +0100

    feat(widget): add initial widget target

---
+++ StepMapWidgets/StepMapWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StepMapWidgetsBundle.swift
+//  StepMapWidgets
+//
+//  Created by Oliver Hnát on 04.12.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StepMapWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        StepMapWidgets()
+    }
+}
